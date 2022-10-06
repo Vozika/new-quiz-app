2e4287742e12abcf734f1c32b48e8fdbcec0a2f0
Sergey Vozika <metasoma@yandex.ru>
2022-10-06 17:52:09 +0300

    Let's start with Redux.

--- src/components/start/Start.jsx
+++ src/components/start/Start.jsx
@@ -12,8 +12,26 @@ import FormLabel from "@mui/material/FormLabel";
 import CardHeader from "@mui/material/CardHeader";
 import CardContent from "@mui/material/CardContent";
 import SettingsIcon from "@mui/icons-material/Settings";
+import Backdrop from "@mui/material/Backdrop";
+import Box from "@mui/material/Box";
+import Modal from "@mui/material/Modal";
+import Fade from "@mui/material/Fade";
 
 import Divider from "@mui/material/Divider";
+
+const style = {
+  textAlign: "center",
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: "min(100%, 700px)",
+  bgcolor: "background.paper",
+  border: "1px solid #bdbdbd",
+  boxShadow: 24,
+  p: 3,
+  
+};
 
 const Start = ({
   Data,
@@ -23,137 +41,166 @@ const Start = ({
   startQuiz,
   setFlip,
 }) => {
+  const [open, setOpen] = React.useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+
   return (
     <div>
       <Typography variant="h1" sx={{ fontSize: "calc(3vw + 30px)" }}>
         Capital Quiz 2.0
       </Typography>
 
-      <br />
-      <Card>
-        <CardHeader
-          sx={{
-            fontSize: "1.3rem",
-            bgcolor: "#bdbdbd",
-            margin: 0,
-            textAlign: "left",
-          }}
-          disableTypography
-          title="OPTIONS"
-          avatar={
-            <Avatar sx={{ bgcolor: "#dd3131" }} aria-label="options">
-              <SettingsIcon />
-            </Avatar>
-          }
-        ></CardHeader>
-        <CardContent>
-          <FormControl sx={{ margin: 0 }}>
-            <FormLabel
-              id="demo-row-radio-buttons-group-label"
-              sx={{ margin: 0 }}
-            >
-              <Typography
+      <Modal
+        aria-labelledby="transition-modal-title"
+        aria-describedby="transition-modal-description"
+        open={open}
+        onClose={handleClose}
+        closeAfterTransition
+        BackdropComponent={Backdrop}
+        BackdropProps={{
+          timeout: 500,
+        }}
+      >
+        <Fade in={open}>
+          <Box sx={style}>
+            <Card sx={{margin: 2}}>
+              <CardHeader
                 sx={{
-                  marginTop: 0,
-                  justifyContent: "center",
-                  alignItems: "center",
                   fontSize: "1.3rem",
-                  fontWeight: "bold",
-                  color: "black",
+                  bgcolor: "#bdbdbd",
+                  margin: 0,
+                  textAlign: "left",
                 }}
-              >
-                Number of questions
-              </Typography>
-            </FormLabel>
-            <RadioGroup
-              row
-              aria-labelledby="demo-row-radio-buttons-group-label"
-              name="row-radio-buttons-group"
-              sx={{
-                margin: 0,
-                justifyContent: "center",
-                alignItems: "center",
-              }}
+                disableTypography
+                title="OPTIONS"
+                avatar={
+                  <Avatar sx={{ bgcolor: "#dd3131" }} aria-label="options">
+                    <SettingsIcon />
+                  </Avatar>
+                }
+              ></CardHeader>
+              <CardContent>
+                <FormControl sx={{ margin: 0 }}>
+                  <FormLabel
+                    id="demo-row-radio-buttons-group-label"
+                    sx={{ margin: 0 }}
+                  >
+                    <Typography
+                      sx={{
+                        marginTop: 0,
+                        justifyContent: "center",
+                        alignItems: "center",
+                        fontSize: "1.3rem",
+                        fontWeight: "bold",
+                        color: "black",
+                      }}
+                    >
+                      Number of questions
+                    </Typography>
+                  </FormLabel>
+                  <RadioGroup
+                    row
+                    aria-labelledby="demo-row-radio-buttons-group-label"
+                    name="row-radio-buttons-group"
+                    sx={{
+                      margin: 0,
+                      justifyContent: "center",
+                      alignItems: "center",
+                    }}
+                  >
+                    <FormControlLabel
+                      value={Data.length}
+                      control={<Radio />}
+                      label={Data.length}
+                      onChange={() => {
+                        setNumberOfQuestions(Data.length);
+                        setSlicedItemsFromData([]);
+                      }}
+                    />
+                    <FormControlLabel
+                      value={15}
+                      control={<Radio />}
+                      label="15"
+                      onChange={() => {
+                        setNumberOfQuestions(15);
+                      }}
+                    />
+                    <FormControlLabel
+                      value={10}
+                      control={<Radio />}
+                      label="10"
+                      onChange={() => {
+                        setNumberOfQuestions(10);
+                      }}
+                    />
+                    <FormControlLabel
+                      value={5}
+                      control={<Radio />}
+                      label="5"
+                      onChange={() => {
+                        setNumberOfQuestions(5);
+                      }}
+                    />
+                  </RadioGroup>
+
+                  <Divider sx={{ margin: "0.5em 0" }} />
+
+                  <FormControlLabel
+                    sx={{
+                      margin: 0,
+                      justifyContent: "center",
+                      alignItems: "center",
+                      fontSize: "1.3rem",
+                      fontWeight: "bold",
+                    }}
+                    disableTypography
+                    control={<Checkbox />}
+                    label="Flip the question"
+                    onChange={() => {
+                      setFlip((prevState) => !prevState);
+                    }}
+                  />
+                  <Typography sx={{ fontSize: "1.3rem", marginBottom: 0 }}>
+                    Now you know the capital. But what about the country?
+                  </Typography>
+
+                  <Divider sx={{ margin: "0.7em 0" }} />
+
+                  <FormControlLabel
+                    sx={{
+                      margin: 0,
+                      justifyContent: "center",
+                      alignItems: "center",
+                      fontSize: "1.3rem",
+                      fontWeight: "bold",
+                    }}
+                    disableTypography
+                    control={<Checkbox />}
+                    label="Show 50/50"
+                    onChange={() => {
+                      setShow5050((prevState) => !prevState);
+                    }}
+                  />
+                  <Typography sx={{ fontSize: "1.3rem", marginBottom: 0 }}>
+                    Let us give you a little help.
+                  </Typography>
+                </FormControl>
+              </CardContent>
+            </Card>
+            <Button
+              variant="contained"
+              size="large"
+              sx={{ height: 60, fontSize: 18, marginTop: 2 }}
+              onClick={startQuiz}
             >
-              <FormControlLabel
-                value={Data.length}
-                control={<Radio />}
-                label={Data.length}
-                onChange={() => {
-                  setNumberOfQuestions(Data.length);
-                  setSlicedItemsFromData([]);
-                }}
-              />
-              <FormControlLabel
-                value={15}
-                control={<Radio />}
-                label="15"
-                onChange={() => {
-                  setNumberOfQuestions(15);
-                }}
-              />
-              <FormControlLabel
-                value={10}
-                control={<Radio />}
-                label="10"
-                onChange={() => {
-                  setNumberOfQuestions(10);
-                }}
-              />
-              <FormControlLabel
-                value={5}
-                control={<Radio />}
-                label="5"
-                onChange={() => {
-                  setNumberOfQuestions(5);
-                }}
-              />
-            </RadioGroup>
+              Start the Quiz!
+            </Button>
+          </Box>
+        </Fade>
+      </Modal>
 
-            <Divider sx={{ margin: "0.5em 0" }} />
-
-            <FormControlLabel
-              sx={{
-                margin: 0,
-                justifyContent: "center",
-                alignItems: "center",
-                fontSize: "1.3rem",
-                fontWeight: "bold",
-              }}
-              disableTypography
-              control={<Checkbox />}
-              label="Flip the question"
-              onChange={() => {
-                setFlip((prevState) => !prevState);
-              }}
-            />
-            <Typography sx={{ fontSize: "1.3rem", marginBottom: 0 }}>
-              Now you know the capital. But what about the country?
-            </Typography>
-
-            <Divider sx={{ margin: "0.7em 0" }} />
-
-            <FormControlLabel
-              sx={{
-                margin: 0,
-                justifyContent: "center",
-                alignItems: "center",
-                fontSize: "1.3rem",
-                fontWeight: "bold",
-              }}
-              disableTypography
-              control={<Checkbox />}
-              label="Show 50/50"
-              onChange={() => {
-                setShow5050((prevState) => !prevState);
-              }}
-            />
-            <Typography sx={{ fontSize: "1.3rem", marginBottom: 0 }}>
-              Let us give you a little help.
-            </Typography>
-          </FormControl>
-        </CardContent>
-      </Card>
+      
 
       <Button
         variant="contained"
@@ -162,6 +209,14 @@ const Start = ({
         onClick={startQuiz}
       >
         Start the Quiz!
+      </Button><br />
+      <Button
+        variant="outlined"
+        size="large"
+        sx={{ height: 60, fontSize: 18, marginTop: 2 }}
+        onClick={handleOpen}
+      >
+        Options
       </Button>
     </div>
   );
